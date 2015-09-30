4450800f119ef7cfe7405eba6511be17298b4d88
pseelinger <patseelinger@gmail.com>
2015-09-30 18:40:47 -0400

    styled a lot better

--- js/script.js
+++ js/script.js
@@ -6,44 +6,12 @@ function setupSlider() {
     fade: true,
     mobileFirst: true,
     adaptiveHeight: true,
-    arrows: false
+    arrows: false,
+    dots: true
   });
-});$('.slider-nav').slick({
-  slidesToShow: 4,
-  asNavFor: '.slider-main',
-  centerMode: true,
-  focusOnSelect: true,
-  responsive: [
-    {
-      breakpoint: 1090,
-      settings: {
-        slidesToShow: 3,
-        centerMode:true,
-        centerPadding: '20px',
-        focusOnSelect: true,
-      }
-    },
-    {
-      breakpoint: 880,
-      settings: {
-        slidesToShow: 2,
-        centerMode: true,
-        centerPadding: '20px',
-        focusOnSelect: true,
-      }
-    },
-    {
-      breakpoint: 500,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        focusOnSelect: true,
-      }
-    }
-  ]
-})};
+});};
 function addQuiz(){
-  var quiz = jQuery('#quiz').quiz('0Arenb9rAosmbdG5GWHFXbWJlN1hTR2ZmN3lZMVZkOHc');
+  var quiz = jQuery('#quiz').quiz('13OM8GsrccKhhbpNU_9wskCYqVDhkrrClrIOadVMihcY');
   setupSlider();
 }
 function addScroll(){

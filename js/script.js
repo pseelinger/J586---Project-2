b564f450030ed5f4df70169f7741e0211f50bf81
pseelinger <patseelinger@gmail.com>
2015-09-30 23:06:55 -0400

    final

--- js/script.js
+++ js/script.js
@@ -18,7 +18,32 @@ function addScroll(){
   jQuery(document).ready(function(){
     jQuery('.scrollbar-inner').scrollbar();
     addQuiz();
+      $('.q-site').qtip({
+    content: $('<iframe src="http://iwsf.com" />'),
+    hide: {
+      delay: 500,
+      fixed: true
+    }
 });
+       $('.q-site-two').qtip({
+    content: $('<iframe src="https://en.m.wikipedia.org/wiki/Cable_skiing" />'),
+    hide: {
+      delay: 500,
+      fixed: true
+    }
+});
+       $('.q-site-three').qtip({
+    content: $('<iframe src="https://en.m.wikipedia.org/wiki/Compression_molding" />'),
+    hide: {
+      delay: 500,
+      fixed: true
+    },
+    position:{
+      my: 'top right',
+      at: 'bottom right'
+    }
+});
+})
 }
 $(document).ready(function(){
   addScroll();
